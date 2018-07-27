925aafcae24a916eb419b4913c55640d1e87e6a9
Rashmi Iyer <hellorashmi100@gmail.com>
2018-07-27 21:43:26 +0000

    add mlab

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
-mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true });
+// mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true });
+mongoose.connect("mongodb://rashmiiyer:bubbles9@ds043942.mlab.com:43942/yelpcampproj", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
